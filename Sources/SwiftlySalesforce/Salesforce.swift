c6e67289ac2846e089d3c274b6430c8e513a252b
Michael Epstein <mike@efactory.com>
2020-05-25 22:51:32 -0500

    Renamed Salesforce.RequestConfig.default back to .shared

--- Sources/SwiftlySalesforce/Salesforce.swift
+++ Sources/SwiftlySalesforce/Salesforce.swift
@@ -35,7 +35,7 @@ open class Salesforce {
         public var authenticateIfRequired: Bool
         public var retries: Int
         
-        public static let `default` = RequestConfig()
+        public static let shared = RequestConfig()
         
         public init(
             version: String = "49.0",
